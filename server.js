9b72e175bb72052412c0819e2f822e99457382c2
유국현 <rnrgus5897@gmail.com>
2021-07-27 17:08:35 +0900

    easylogin 구현

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+if( process.env.SESSION_SCRIPT !== 'production'){
+    require('dotenv').config();
+};
+
+
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
+const methodOverride = require('method-override')
+//passport
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const initializePassport = require('./passport-config');
+
+initializePassport(passport , email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+);
 //bcrypt 해쉬 비밀번호 사용
 const bcrypt = require('bcrypt');
 //body-parser
 const bodyParser = require('body-parser');
+
+//override 미들웨어
+app.use(methodOverride('_method'));
 
 //views setting
 app.set('views' , 'views');
@@ -12,28 +31,41 @@ app.set('view engine' , 'ejs');
 //body-parser 미들웨어 등록
 app.use(bodyParser.urlencoded({extended: false}));
 
+//flash , session 미들웨어등록
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SCRIPT,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+//데이터 값을 넣기 위해서 배열 만들기
 const users = [];
 
-app.get('/' , (req , res) => {
-    res.render('index' , {name: '유국현'});
+app.get('/' , checkAuthenticated , (req , res) => {
+    res.render('index' , {name: req.user.name});
 })
 
 //login
-app.get('/login' , (req , res) => {
+app.get('/login' ,checkNotAuthenticated, (req , res) => {
     res.render('login');
 });
 
-app.post('/login' , (req , res) => {
-
-})
+app.post('/login' , checkNotAuthenticated,  passport.authenticate('local' , {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+}))
 
 //register
-app.get('/register' , (req , res) => {
+app.get('/register', checkNotAuthenticated, (req , res) => {
     res.render('register');
-});
-
+})
 //post
-app.post('/register' , async (req , res) => {
+app.post('/register' ,checkNotAuthenticated, async (req , res) => {
     try{
         const hashedPassword = await bcrypt.hash(req.body.password , 10);
         users.push({
@@ -48,10 +80,27 @@ app.post('/register' , async (req , res) => {
     }
 });
 
+app.delete('/login' , (req , res) => {
+    req.logOut();
+    res.redirect('/login');
+})
 
 
+function checkAuthenticated(req , res , next){
+    if(req.isAuthenticated()){
+        return next();
+    }
 
-const port = process.env.PORT || 5000;
+    res.redirect('/login')
+}
+function checkNotAuthenticated(req , res , next){
+    if(req.isAuthenticated()){
+        return res.redirect('/')
+    }
+    next();
+}
+
+
 
 app.listen(port , () => {
     console.log(`${port}포트 포트로 이동중....`)
